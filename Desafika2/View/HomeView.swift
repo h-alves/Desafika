3bc2ed14e02275639c2361ceb393b19d472f1e07
Henrique Alves <henriqueasemmer@gmail.com>
2023-10-12 16:08:14 -0300

    Views created

--- Desafika2/View/HomeView.swift
+++ Desafika2/View/HomeView.swift
@@ -9,7 +9,43 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 28) {
+            
+            // Texto
+            VStack(spacing: 8) {
+                Text("Prontos para um novo desafio?")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                Text("Selecione as categorias que desejar e clique no rostinho para gerar um desafio!")
+                    .font(.callout)
+                    .multilineTextAlignment(.center)
+            }
+            .frame(maxWidth: 300)
+            
+            // Categorias
+            VStack {
+                
+            }
+            
+            // Raio de distância
+            VStack {
+                Text("Sugerir lugares em um raio de:")
+                    .font(.footnote)
+                
+                // Slider
+            }
+            
+            // Botão
+            VStack {
+                // Componente de botão
+                
+                Text("Gerar desafio")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+            }
+        }
+        .padding(.horizontal, 32)
     }
 }
 
--- Desafika2/View/OnBoardingView.swift
+++ Desafika2/View/OnBoardingView.swift
@@ -9,7 +9,80 @@ import SwiftUI
 
 struct OnBoardingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 200) {
+            // Textos
+            VStack(spacing: 36) {
+                Text("Bem vindo(a)!")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                
+                VStack(spacing: 16) {
+                    HStack {
+                        Image(systemName: "sparkles")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                        
+                        VStack {
+                            Text("Descubra novas ideias")
+                                .font(.title2)
+                                .fontWeight(.bold)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            Text("Receba ideias diferenciadas de atividades para fazer em casal")
+                                .font(.callout)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                        }
+                    }
+                    
+                    HStack {
+                        Image(systemName: "building.2.crop.circle.fill")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                        
+                        VStack {
+                            Text("Sugestão de lugares")
+                                .font(.title2)
+                                .fontWeight(.bold)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            Text("Lugares são sugeridos de forma customizada com cada desafio")
+                                .font(.callout)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                        }
+                    }
+                    
+                    HStack {
+                        Image(systemName: "bookmark.circle.fill")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                        
+                        VStack {
+                            Text("Salve seu progresso")
+                                .font(.title2)
+                                .fontWeight(.bold)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            Text("Conclua desafios e tenha um histórico das suas atividades")
+                                .font(.callout)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                        }
+                    }
+                }
+            }
+            
+            // Botão
+            Button {
+               print("a")
+            } label: {
+                Text("Começar")
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.white)
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 16)
+                    .background(.red)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+            }
+            
+        }
+        .padding(.horizontal, 48)
     }
 }
 
