51361a4b8e0ead1a9cfa4f090e0c59be0efbd7aa
Henrique Alves <henriqueasemmer@gmail.com>
2023-10-12 15:31:37 -0300

    Initial Commit

---
+++ Desafika2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Desafika2
+//
+//  Created by Henrique Semmer on 12/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Desafika2/Desafika2App.swift
@@ -0,0 +1,17 @@
+//
+//  Desafika2App.swift
+//  Desafika2
+//
+//  Created by Henrique Semmer on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Desafika2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
